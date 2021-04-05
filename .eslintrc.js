88714e8c5b62c4d63af284abf64d98c86ab7885f
wusagi24 <wusagi24@lunalabo.net>
2021-04-05 12:39:29 +0900

    Modify Config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,33 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  env: {
+    node: true,
+    es2020: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
+  overrides: [
+    {
+      files: ['*.ts'],
+      env: {},
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+      },
+      plugins: [
+        '@typescript-eslint',
+      ],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+      ],
+    }
+  ]
 };
